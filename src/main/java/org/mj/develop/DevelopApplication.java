a4037a059b302046bcdc2b0f8c222f025d38b257
owner <alswns6327@naver.com>
2023-12-07 10:49:08 +0900

    Initial commit

---
+++ src/main/java/org/mj/develop/DevelopApplication.java
@@ -0,0 +1,11 @@
+package org.mj.develop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopApplication {
+    public static void main(String[] args ){
+        SpringApplication.run(DevelopApplication.class, args);
+    }
+}
